31d2e0f62e25a56048ae7538c1a423f98a24492a
Navya Manjari <navyamanjari.hyd@gmail.com>
2021-02-27 10:38:37 +0530

    new info

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path=require('path');
-
+const request=require('request');
 const PORT=process.env.PORT || 5000;
 
+
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_8dfb83b25a2343f1add455c11da00a4a',{json : true},(err,res,body) =>{
+
+if(err)
+{
+	return console.log(err);
+}
+console.log(body);
+if(res.statusCode===200)
+{
+	console.log(body);
+};
+});
+//api key pk_8dfb83b25a2343f1add455c11da00a4a
 //SET HANDLEBARS MIDDLEWARE
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -22,6 +36,10 @@ app.get('/', function (req, res) {
     });
 });
 
+//create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
 
 app.use(express.static(path.join(__dirname,'public')));
 
